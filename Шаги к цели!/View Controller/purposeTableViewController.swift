97eb22225ec9aeaf5e29a6efb21c39746ff13708
Sergey <piolersa85@mail.ru>
2021-08-04 18:56:17 +0200

    32

--- Шаги к цели!/View Controller/purposeTableViewController.swift
+++ Шаги к цели!/View Controller/purposeTableViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class purposeTableViewController: UITableViewController {
+    
+    
+    
+
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +31,42 @@ class purposeTableViewController: UITableViewController {
            
       10
        }
+    
+ 
 
        
        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = " Cell Index: \(indexPath.row)"
+      //  let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+      
+       
+        cell.textLabel?.text = " Научиться отжиматься:"
+        cell.detailTextLabel?.text = " 50 раз в день!"
+        
+        
+       
+        cell.imageView?.image = #imageLiteral(resourceName: "зевс")
+        cell.imageView?.layer.cornerRadius = tableView.rowHeight / 2
+        cell.imageView?.clipsToBounds = true
         
            return cell
        }
     
+    
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        
+    return 100
     }
-       
-
+       // выбор ячейки когда нажимаем
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+    }
+    
+    
+    
+    
+    
     
     // MARK: - Navigation
 
