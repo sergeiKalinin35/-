5a667c8032a5fcf2f6814434df1c942179c67fd6
Sergey <piolersa85@mail.ru>
2021-08-04 20:54:41 +0200

    scheduleTest

--- Шаги к цели!/View Controller/editingViewController.swift
+++ Шаги к цели!/View Controller/editingViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class editingViewController: UIViewController {
+    
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var bodyField: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    
+    
+    
+    public var completion: ((String, String, Date) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+}
+    
+    
+    @IBAction func didTapSaveButton() {
+        
+        if let titleText = titleField.text, !titleText.isEmpty,
+           let bodyText = bodyField.text, !bodyText.isEmpty {
+            
+  //          let targetDate = datePicker.date
+            
+            
+            
+        }
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }
--- Шаги к цели!/View Controller/purposeTableViewController.swift
+++ Шаги к цели!/View Controller/purposeTableViewController.swift
@@ -4,50 +4,100 @@
 //
 //  Created by Sergey on 01.08.2021.
 //
-
+import UserNotifications
 import UIKit
+
+
+
 
 class purposeTableViewController: UITableViewController {
     
     
+    var models = [MyReminder]()
     
-
-    
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      
-
-    
+        
+        
     }
+        
+        
+        @IBAction func didTapAdd() {
+            
+            
+            
+            
+            
+            
+    }
+
+        @IBAction func didTapTest() {
+            
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { success, error in
+                if success{
+                    self.scheduleTest()
+                    // test
+                }
+                else if error != nil {
+                    
+                    print("error occurred")
+                    
+                }
+                
+            })
+            
+            
+        
+}
+        //объект содержимое уведомление
+    
+    func scheduleTest() {
+        let content = UNMutableNotificationContent()
+        content.title = "Лили 🦥, поцелуй меня 😘"
+        content.sound = .default
+        content.body = "Это я 🐣 "
+        
+        
+        let targetDate = Date().addingTimeInterval(20)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second],
+                                                                        from: targetDate) ,
+                                                   repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print("something went wrong")
+                
+            }
+            
+        })
+    }
+    
+
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
            
-      10
+        return models.count
+        
        }
     
- 
-
-       
-       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      //  let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        
+        
+        //    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
       
-       
-        cell.textLabel?.text = " Научиться отжиматься:"
-        cell.detailTextLabel?.text = " 50 раз в день!"
+       //   cell.textLabel?.text = " Научиться отжиматься:"
+     //   cell.detailTextLabel?.text = " 50 раз в день!"
         
-        
-       
-        cell.imageView?.image = #imageLiteral(resourceName: "зевс")
-        cell.imageView?.layer.cornerRadius = tableView.rowHeight / 2
-        cell.imageView?.clipsToBounds = true
+    //    cell.imageView?.image = #imageLiteral(resourceName: "зевс")
+    //    cell.imageView?.layer.cornerRadius = tableView.rowHeight / 2
+    //    cell.imageView?.clipsToBounds = true
         
            return cell
        }
@@ -76,4 +126,12 @@ class purposeTableViewController: UITableViewController {
     }
    
 
+}
+
+
+struct MyReminder {
+    let title: String
+    let date: Date
+    let identifier: String
+    
 }
